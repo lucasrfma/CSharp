5f4f0345459070ee5c148cf52f7b21b04ceb985f
Lucas Romero de Andrade <78609723+lucasrfma@users.noreply.github.com>
2021-04-02 16:00:01 -0300

    Atividade bootcamp .Net Fundamentals

---
+++ DigitalInnovationOne/Desafios/Introducao a Programacao com C#/Dividindo X por Y/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dividindo_X_por_Y
+{
+    class Program
+    {
+        static void Main() {
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++) 
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0) {
+                    Console.WriteLine("divisao impossivel");
+                } else {
+                    double divisao = X / Y;
+                    Console.WriteLine(divisao.ToString("N1"));
+                }
+            }
+        }
+    }
+}
