82045b933ac2ac019461bce1980c75de3a11a44d
Lucas Romero de Andrade <78609723+lucasrfma@users.noreply.github.com>
2021-05-04 14:28:19 -0300

    ch

--- DigitalInnovationOne/Desafios/Solucao de problemas com .NET/ValidacaoNotas/Program.cs
+++ DigitalInnovationOne/Desafios/Solucao de problemas com .NET/ValidacaoNotas/Program.cs
@@ -9,34 +9,32 @@ namespace ValidacaoNotas
             int continua = 1;
             while ( continua == 1) 
             {
-                float nota1 = -1;
-                float nota2 = -1;
-                float media;
-                bool sucesso = false;
+                double nota1 = -1;
+                double nota2 = -1;
+                double media;
                 
-                // Console.WriteLine($"Insira a nota da primeira avaliação.");
+                bool sucesso = false;
                 while(true)
                 {
-                    sucesso = float.TryParse(Console.ReadLine(), out nota1);
-                    if(!sucesso || (nota1 < 0.0 || nota1 > 10.0))
+                    sucesso = double.TryParse(Console.ReadLine(), out nota1);
+                    if(!sucesso || nota1 < 0.0 || nota1 > 10.0)
                         Console.WriteLine($"nota invalida");
                     else
                         break;
                 }
 
                 sucesso = false;
-                // Console.WriteLine($"Insira a nota da segunda avaliação.");
                 while(true)
                 {
-                    sucesso = float.TryParse(Console.ReadLine(), out nota2);
-                    if(!sucesso || (nota2 < 0.0 || nota2 > 10.0) )
+                    sucesso = double.TryParse(Console.ReadLine(), out nota2);
+                    if(!sucesso || nota2 < 0.0 || nota2 > 10.0)
                         Console.WriteLine($"nota invalida");
                     else
                         break;
                 }
 
                 media = (nota1 + nota2)/2;
-                Console.WriteLine("media = "+media.ToString("N2"));
+                Console.WriteLine("media = "+media.ToString("n2"));
                 
                 sucesso = false;
                 while( !sucesso || (continua != 1 && continua != 2) )
@@ -48,4 +46,5 @@ namespace ValidacaoNotas
         }
     }
 }
+
 
